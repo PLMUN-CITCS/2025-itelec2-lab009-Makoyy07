ec701ac8273dca03f0626fd5de42c4548d9b48da
Makoyy07 <>
2025-02-13 22:45:03 +0800

    Last Sync: 2025-02-13 14:45:03 (Mobile)

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,12 +1,14 @@
 user_input = input("Enter a number: ")
 number = int(user_input)
-    
-    try:
-    
-if number % 2 == 0:
-    print("The number", number, "is Even.")
-else:
-    print("The number", number, "is Odd.")
-    
+
+try:
+    user_input = input("Enter a number: ")
+    number = int(user_input)  # Sinusubukang i-convert sa integer
+
+    if number % 2 == 0:
+        print("The number", number, "is Even.")
+    else:
+        print("The number", number, "is Odd.")
+
 except ValueError:
-    print("Invalid input. Please enter an integer.")
+    print("Invalid input. Please enter an integer.")  # Lalabas kung hindi valid ang input
